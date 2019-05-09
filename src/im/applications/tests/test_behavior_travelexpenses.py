0f7329ac4918bcd734e91a8627be24adc62110f1
adriana-rv <adriana.rv@gmail.com>
2019-05-09 10:51:24 -0500

    Add behavior: travelexpenses

---
+++ src/im/applications/tests/test_behavior_travelexpenses.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from im.applications.behaviors.travelexpenses import ITravelexpensesMarker
+from im.applications.testing import IM_APPLICATIONS_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class TravelexpensesIntegrationTest(unittest.TestCase):
+
+    layer = IM_APPLICATIONS_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_travelexpenses(self):
+        behavior = getUtility(IBehavior, 'im.applications.travelexpenses')
+        self.assertEqual(
+            behavior.marker,
+            ITravelexpensesMarker,
+        )
