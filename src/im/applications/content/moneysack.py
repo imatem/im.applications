a234655d4f720279111df48ac066277bfc9bbea4
adriana-rv <adriana.rv@gmail.com>
2019-05-13 16:16:50 -0500

    Add new fields

--- src/im/applications/content/moneysack.py
+++ src/im/applications/content/moneysack.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from im.applications import _
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from zope.interface import Invalid
+from zope.interface import invariant
 from zope.interface import implementer
 
 
@@ -16,45 +14,34 @@ from zope.interface import implementer
 class IMoneysack(model.Schema):
     """ Marker interface and Dexterity Python Schema for Moneysack
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    title = schema.TextLine(
+        title=_(u'label_applications_moneysack_title', default=u'Title of sack'),
+        required=True,
+    )
 
-    # model.load('moneysack.xml')
+    start = schema.Date(
+        title=_(u'label_applications_moneysack_start_date', default=u'Start date of the activity'),
+        required=True,
+    )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    end = schema.Date(
+        title=_(u'label_applications_moneysack_end_date', default=u'End date of the activity'),
+        required=True,
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    amount = schema.Float(
+        title=_(u'label_applications_moneysack_amount', u'Approved Amount'),
+        description=_(u'help_applications_maneysack_amount', u'Amount when the solicitud was created'),
+        required=True,
+        min=1.0,
+        # max=25000.0,
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    @invariant
+    def validateDateFields(data):
+        if data.end < data.start:
+            message = 'Invalid Dates: the Star Date must be greater that End Date, please correct it.'
+            raise Invalid(_('label_applications_error_datesbefore', default=message))
 
 
 @implementer(IMoneysack)
