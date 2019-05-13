ba77421257f6e2ec5d33d396e15726804320997b
adriana-rv <adriana.rv@gmail.com>
2019-05-13 16:33:09 -0500

    Add real fields for colouio activity

--- src/im/applications/content/coloquio_application.py
+++ src/im/applications/content/coloquio_application.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from im.applications import _
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Item
@@ -6,7 +7,7 @@ from plone.dexterity.content import Item
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
@@ -16,45 +17,21 @@ from zope.interface import implementer
 class IColoquioApplication(model.Schema):
     """ Marker interface and Dexterity Python Schema for ColoquioApplication
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('coloquio_application.xml')
+    title = schema.TextLine(
+        title=_(u'label_applications_coloquio_title', default=u'Speaker Name'),
+        required=True,
+    )
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    institution = schema.TextLine(
+        title=_(u'label_applications_coloquio_institution', default=u'Speaker Institution'),
+        required=True,
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    exposition_date = schema.Date(
+        title=_(u'label_applications_coloquio_exposition_date', default=u'Exposition Date'),
+        required=True,
+    )
 
 
 @implementer(IColoquioApplication)
