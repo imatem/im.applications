c41a1bd1255d7b020b8b38cb53185597a9f241cd
adriana-rv <adriana.rv@gmail.com>
2019-05-20 10:50:51 -0500

    Add dates for adminitrate the comissions dates

--- src/im/applications/content/coloquio_application.py
+++ src/im/applications/content/coloquio_application.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from im.applications import _
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.autoform import directives
 from plone.dexterity.content import Item
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
@@ -30,6 +30,28 @@ class IColoquioApplication(model.Schema):
 
     exposition_date = schema.Date(
         title=_(u'label_applications_coloquio_exposition_date', default=u'Exposition Date'),
+        required=True,
+    )
+
+    directives.read_permission(specialc_date='Solicitud: Comision Revisa Solicitud')
+    directives.write_permission(specialc_date='Solicitud: Comision Revisa Solicitud')
+    specialc_date = schema.Date(
+        title=_(u'label_applications_amount_specialc_date', default=u'Special Comision Date'),
+        required=True,
+    )
+
+    directives.read_permission(minute='Solicitud: Consejo Revisa Solicitud')
+    directives.write_permission(minute='Solicitud: Consejo Revisa Solicitud')
+    minute = schema.TextLine(
+        title=_(u'label_applications_minute', default=u'Minute Number'),
+        description=_(u'help_applications_minute', default=u'Minute of Consejo Interno'),
+        required=False,
+    )
+
+    directives.read_permission(internalc_date='Solicitud: Consejo Revisa Solicitud')
+    directives.write_permission(internalc_date='Solicitud: Consejo Revisa Solicitud')
+    internalc_date = schema.Date(
+        title=_(u'label_applications_amount_internalc_date', default=u'Consejo Interno Date'),
         required=True,
     )
 
