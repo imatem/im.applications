503ac2d93ccda8ac9c403afa876b688fe52530c1
adriana-rv <adriana.rv@gmail.com>
2019-06-21 16:05:39 -0500

    Add scripts to prepare objects in the workflows transsitions

---
+++ src/im/applications/profiles/default/workflows/im_applications_workflow/scripts/prepare_to_consejo.py
@@ -0,0 +1,10 @@
+## Script (Python) "prepare_to_consejo"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+context.prepareToConsejo()
